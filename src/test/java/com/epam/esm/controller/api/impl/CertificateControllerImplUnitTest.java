cd95284f97fecca1f97728af9550b25ef399ce55
Strider-by <hush.interlocutor@yandex.ua>
2022-05-10 02:19:40 +0300

    added certificate controller unit tests

--- src/test/java/com/epam/esm/controller/api/impl/CertificateControllerImplUnitTest.java
+++ src/test/java/com/epam/esm/controller/api/impl/CertificateControllerImplUnitTest.java
@@ -1,33 +1,40 @@
 package com.epam.esm.controller.api.impl;
 
+import com.epam.esm.controller.api.exception.CertificateNotFoundException;
 import com.epam.esm.model.Certificate;
 import com.epam.esm.model.dto.CertificateCreateDto;
 import com.epam.esm.service.CertificateService;
+import com.epam.esm.util.ContentStringBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-//@RunWith(SpringRunner.class)
 @SpringBootTest
 class CertificateControllerImplUnitTest {
 
-//    @Autowired
+//    @Autowired // todo: work without autowiring?
     private MockMvc mockMvc;
     private CertificateService service;
     @Autowired
@@ -47,34 +54,114 @@ class CertificateControllerImplUnitTest {
 
     @BeforeEach
     void setUp() {
-        service = context.getBean("certificatesServiceMock", CertificateService.class);
-//        CertificatesController controller = context.getBean(CertificatesController.class);
+        service = Mockito.mock(CertificateService.class);
         mockMvc = MockMvcBuilders.standaloneSetup(new CertificateControllerImpl(service))
                 .build();
     }
 
     @Test
     void getAllCertificates() throws Exception {
+        int pageNumber = 0;
+        int pageSize = 10;
+        List<Certificate> expectedContent = createNCertificates(pageSize);
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        long elementsTotal = 25;
+
+        when(service.getAllCertificates(any())).thenReturn(
+                new PageImpl<>(expectedContent, pageable, elementsTotal));
+        
         mockMvc.perform(get("/certificates/"))
                 .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                // todo: remove? this doesn't seems like it should be part of this test
+                .andExpect(jsonPath("$.elements_on_current_page").value(pageSize));
+    }
+
+    @Test
+    void getCertificate_success() throws Exception {
+        long id = 1L;
+        when(service.getCertificate(id)).thenReturn(new Certificate());
+
+        mockMvc.perform(get("/certificates/{id}", id))
+                .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
     }
 
     @Test
-    void getCertificate() {
+    void getCertificate_fail_notFound() throws Exception {
+        long id = 404L;
+        mockMvc.perform(get("/certificates/{id}", id))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                // todo: should it be part of this test?
+                .andExpect(jsonPath("$.code").value(HttpStatus.NOT_FOUND.value()));
     }
 
     @Test
-    void createCertificate() {
+    void createCertificate() throws Exception {
+        long id = 1L;
+        String name = "new_name";
+        int price = 400;
+
+        CertificateCreateDto dto = new CertificateCreateDto();
+        dto.setName(name);
+        dto.setPrice(price);
+
+        Certificate expected = new Certificate();
+        expected.setId(id);
+        expected.setName(name);
+        expected.setPrice(price);
+
+        when(service.createCertificate(dto)).thenReturn(expected);
+
+        String content = new ContentStringBuilder()
+                .add("name", name)
+                .add("price", price)
+                .add("id", id)
+                .build();
+
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.post("/certificates")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                .characterEncoding("UTF-8")
+                .content(content);
+
+
+        this.mockMvc.perform(builder)
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(id))
+                .andExpect(jsonPath("$.name").value(name))
+                .andExpect(jsonPath("$.price").value(price));
     }
 
     @Test
-    void deleteCertificate() {
+    void deleteCertificate_success() throws Exception {
+        long id = 1L;
+        mockMvc.perform(delete("/certificates/{id}", id))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.code").value(HttpStatus.OK.value()));
+    }
+
+    @Test
+    void deleteCertificate_fail_notFound() throws Exception {
+        long id = 404L;
+        doThrow(new CertificateNotFoundException(id)).when(service).deleteCertificate(id);
+        mockMvc.perform(delete("/certificates/{id}", id))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.code").value(HttpStatus.NOT_FOUND.value()));
     }
 
 
-    private void add3certificates() {
-        IntStream.of(1, 2, 3).forEach(val -> service.createCertificate(new CertificateCreateDto()));
+    
+    private List<Certificate> createNCertificates(int n) {
+        List<Certificate> certificates = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            Certificate certificate = new Certificate();
+            certificates.add(certificate);
+        }
+        return certificates;
     }
 
 }
---
+++ src/test/java/com/epam/esm/util/ContentStringBuilder.java
@@ -0,0 +1,22 @@
+package com.epam.esm.util;
+
+import com.epam.esm.model.dto.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContentStringBuilder {
+    private List<Pair<String, Object>> chunks = new ArrayList<>();
+
+    public ContentStringBuilder add(String name, Object value) {
+        chunks.add(new Pair<>(name, value));
+        return this;
+    }
+
+    public String build() {
+        return chunks.stream()
+                .map(pair -> String.format("%s=%s", pair.getFirstValue(), pair.getSecondValue().toString()))
+                .reduce((part1, part2) -> String.join("&", part1, part2))
+                .orElse("");
+    }
+}
